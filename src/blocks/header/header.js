1a5cea5749902252597fb7da8990aaea4fb4e5e0
andrew.s.dterra <a.shirin@dterra.eu>
2022-03-25 00:33:06 +0300

    fix

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,12 +1,12 @@
 export default function headerScripts() {
-  const burgerIcon = document.querySelector('.svg-icon--icon_burger')
+  const burgerIcon = document.querySelector('.header__burger-icon')
   const burger = document.querySelector('.header__burger')
   burgerIcon.addEventListener('click', function () {
     burger.classList.add('header__burger--visible')
     document.documentElement.classList.add('no-scroll')
   })
 
-  const closeBurgerIcon = document.querySelector('.svg-icon--icon_close')
+  const closeBurgerIcon = document.querySelector('.header__burger-close')
   closeBurgerIcon.addEventListener('click', function () {
     burger.classList.remove('header__burger--visible')
     document.documentElement.classList.remove('no-scroll')
