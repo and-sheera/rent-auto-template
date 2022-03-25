37ceeabf9ea0aa508b276e4050b2aa4bde701ed5
andrew.s.dterra <a.shirin@dterra.eu>
2022-03-26 01:39:01 +0300

    added card-auto page

--- src/blocks/_ui/ui-tooltip/ui-tooltip.js
+++ src/blocks/_ui/ui-tooltip/ui-tooltip.js
@@ -1,6 +1,21 @@
 import tippy from 'tippy.js'
 
 export default function uiTooltip() {
-  const tooltips = document.querySelectorAll('.ui-tooltip button')
-  tippy(tooltips)
+  const tooltips = document.querySelectorAll('.ui-tooltip .ui-tooltip__icon')
+  tippy(tooltips, {
+    content(reference) {
+      const content = reference.nextElementSibling
+      return content.innerHTML
+    },
+    allowHTML: true,
+    appendTo: 'parent',
+    trigger: 'mouseenter focus',
+    interactive: true,
+    theme: 'ui-tooltip',
+    offset: [-100, 10],
+    maxWidth: 300
+  })
+  $(document).on('click', '.ui-tooltip__btn', function (event) {
+    event.preventDefault()
+  })
 }
--- src/blocks/goods/goods.js
+++ src/blocks/goods/goods.js
@@ -1,10 +1,10 @@
 export default function goodsBlock() {
-  if (document.querySelector('.goods')) {
-    const goodsUnvisibleItems = [...document.querySelectorAll('.goods__item--mb-unvisible')]
+  if (document.querySelector('.goods .goods__more-btn')) {
+    const goodsUnvisibleItems = [...document.querySelectorAll('.goods-item--mb-unvisible')]
     const showMoreButton = document.querySelector('.goods__more-btn')
     showMoreButton.addEventListener('click', function () {
       for (const element of goodsUnvisibleItems) {
-        element.classList.remove('goods__item--mb-unvisible')
+        element.classList.remove('goods-item--mb-unvisible')
       }
       showMoreButton.classList.add('goods__more-btn--hidden')
     })
